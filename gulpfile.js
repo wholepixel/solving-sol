e3bd13141981518fcf408f6d871cdfda91f3b9f0
Paul Smith <paul@blinkylights.org>
2016-11-23 05:02:43 -0500

    Solution for #11 by blinkylights23

--- gulpfile.js
+++ gulpfile.js
@@ -10,17 +10,9 @@ var gulp           = require('gulp'),
 
 //Set up simple server w/live reload
 gulp.task('serve', function() {
-
   server.use(livereload());
-
   server.use(express.static('./'));
-
-  //static server
   server.listen(serverport);
-
-  //live reload
-  lrserver.listen();
-
 });
 
 // Watch for changes on files. Ignore node_modules.
