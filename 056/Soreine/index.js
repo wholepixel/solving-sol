e634d01d8867994f67122b7e875c83560d39621e
Soreine <soreine.plume@gmail.com>
2018-10-22 22:49:35 +0200

    Make 4 squares

--- 056/Soreine/index.js
+++ 056/Soreine/index.js
@@ -4,31 +4,45 @@
   A square is divided horizontally and vertically into four equal parts, each with lines in four directions superimposed progressively.
  */
 
+function onResize(event) {
+  draw();
+}
+
 function draw() {
   project.clear();
 
   var phi = (1 + Math.sqrt(5)) / 2;
-  var height = Math.min(view.size.width, view.size.height) / phi;
+  var height = Math.min(view.size.width, view.size.height) / phi / 2;
 
-  fillSquareWithLines(view.center, height);
-  fillSquareWithLines(view.center + 20, height);
-  fillSquareWithLines(view.center + 40, height);
+  var topLeft = view.center - height;
+   
+  // North west
+  fillSquareWithLines(topLeft  + new Point(0, 0), height);
+
+  // North east
+  fillSquareWithLines(topLeft + new Point(height, 0), height);
+
+  // South west
+  fillSquareWithLines(topLeft + new Point(0, height), height);
+
+  // South east
+  fillSquareWithLines(topLeft + new Point(height, height), height);
 }
 
 function fillSquareWithLines(
-  center, // Point
+  corner, // Point
   height, // Number
   angle // Number
 ) {
   var size = new Size(height, height);
-  var square = new Rectangle(center - size.width / 2, size);
+  var square = new Rectangle(corner, size);
 
   var path = new Path.Rectangle(square);
   path.strokeColor = "black";
 
-  
-}
-
-function onResize(event) {
-  draw();
+  for(var i in [0, 1, 2]) {
+    var from = new Point()
+    var linePath = new Path.Rectangle(square);
+    path.strokeColor = "red";
+  }
 }
