ad901ca6b107070d9e65fc524ceac780d18c7701
melissa lopes <melissalopesm@yahoo.com.br>
2022-01-20 15:54:20 -0300

    later

---
+++ 011/Melissalopesm/sketch.js
@@ -0,0 +1,11 @@
+// /## Wall Drawing #11 (1969)
+
+// A wall divided horizontally and vertically into four equal parts. Within each part, three of the four kinds of lines are superimposed.
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
